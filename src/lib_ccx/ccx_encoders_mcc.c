da46b5a378860f1c4175d29fee5d1d8123973a37
Rob Taylor <rob_taylor@comcast.com>
2019-08-09 17:59:47 -0600

    Added the bail at 20.

--- src/lib_ccx/ccx_encoders_mcc.c
+++ src/lib_ccx/ccx_encoders_mcc.c
@@ -31,6 +31,14 @@ static void compress_data( uint8* data_ptr, uint16 num_elements, uint8* out_data
 static void byte_to_ascii( uint8 hex_byte, uint8* msn, uint8* lsn );
 static void write_string( int fh, char* string );
 
+static uint8 dtvccPacket[DTVCC_MAX_PACKET_LENGTH];
+static uint8 dtvccPacketLength;
+static boolean captionTextFound;
+
+static void DecodeCaption( ccx_mcc_caption_time, unsigned char*, int );
+static void processDtvccPacket( ccx_mcc_caption_time* );
+static void processLine21Packet( ccx_mcc_caption_time*, uint8, uint8 );
+
 boolean mcc_encode_cc_data( struct encoder_ctx *enc_ctx, struct lib_cc_decode *dec_ctx, unsigned char *cc_data, int cc_count ) {
     ASSERT(cc_data);
     ASSERT(enc_ctx);
@@ -57,6 +65,18 @@ boolean mcc_encode_cc_data( struct encoder_ctx *enc_ctx, struct lib_cc_decode *d
         LOG("Captions start at: %02d:%02d:%02d:%02d / %02d:%02d:%02d,%03d",
             caption_time.hour, caption_time.minute, caption_time.second, frame_number,
             caption_time.hour, caption_time.minute, caption_time.second, caption_time.millisecond);
+        dtvccPacketLength = 0;
+        captionTextFound = CCX_FALSE;
+    }
+
+    if( captionTextFound == CCX_FALSE ) {
+        DecodeCaption(caption_time, cc_data, cc_count);
+    }
+
+    if( (captionTextFound == CCX_FALSE) && (caption_time.minute >= 20)  ) {
+        LOG("Bailing. No Caption found as of %02d:%02d:%02d,%03d", caption_time.hour,
+            caption_time.minute, caption_time.second, caption_time.millisecond);
+        exit(10);
     }
 
 #if MORE_DEBUG
@@ -621,3 +641,206 @@ static void byte_to_ascii( uint8 hex_byte, uint8* msn, uint8* lsn ) {
 static void write_string( int fh, char* string ) {
     write(fh, string, strlen(string));
 }  // write_string()
+
+//-------    Parse for Caption.
+
+static void DecodeCaption( ccx_mcc_caption_time captionTime, unsigned char *cc_data, int cc_count ) {
+    for( int loop = 0; loop < cc_count; loop = loop + 3 ) {
+        boolean ccValid = ((cc_data[loop] & CC_CONSTR_CC_VALID_FLAG_MASK) == CC_CONSTR_CC_VALID_FLAG_SET);
+        uint8 ccType = cc_data[loop] & CC_CONSTR_CC_TYPE_MASK;
+
+        switch( ccType ) {
+            case CEA608E_LINE21_FIELD_1_CC:
+            case CEA608E_LINE21_FIELD_2_CC:
+                processLine21Packet(&captionTime, (cc_data[loop + 1] & LINE_21_PARITY_MASK),
+                                                  (cc_data[loop + 2] & LINE_21_PARITY_MASK));
+                break;
+            case DTVCCC_CHANNEL_PACKET_START:
+                processDtvccPacket(&captionTime);
+                dtvccPacketLength = 0;
+            case DTVCCC_CHANNEL_PACKET_DATA:
+                if (ccValid == CCX_TRUE) {
+                    if ((dtvccPacketLength + 2) < DTVCC_MAX_PACKET_LENGTH) {
+                        dtvccPacket[dtvccPacketLength++] = cc_data[loop + 1];
+                        dtvccPacket[dtvccPacketLength++] = cc_data[loop + 2];
+                    }
+                }
+                break;
+        }
+    }
+}  // DecodeCaption()
+
+static void processLine21Packet( ccx_mcc_caption_time* captionTimePtr, uint8 ccData1, uint8 ccData2 ) {
+    if( ((ccData1 == GLOBAL_CTRL_CODE_CC1) || (ccData1 == GLOBAL_CTRL_CODE_CC2) ||
+         (ccData1 == GLOBAL_CTRL_CODE_CC3) || (ccData1 == GLOBAL_CTRL_CODE_CC4))
+        && ((ccData2 & GLOBAL_CTRL_CMD_MASK) == ccData2)) {
+        return;
+    }
+
+    if( ((ccData1 == CMD_MIDROW_BG_CHAN_1_3) || (ccData1 == CMD_MIDROW_FG_CHAN_1_3) ||
+         (ccData1 == CMD_MIDROW_BG_CHAN_2_4) || (ccData1 == CMD_MIDROW_FG_CHAN_2_4))
+        && ((ccData2 & MIDROW_CODE_MASK) == ccData2)) {
+        return;
+    }
+
+    if( ((ccData1 == CMD_TAB_HI_CC_1_3) || (ccData1 == CMD_TAB_HI_CC_2_4))
+        && ((ccData2 & TAB_CMD_MASK) == ccData2)) {
+        return;
+    }
+
+    if( (ccData1 & PAC_CC1_MASK) == ccData1 ) {
+        return;
+    }
+
+    if( ((ccData1 == SPCL_NA_CHAR_SET_CH_1_3) || (ccData1 == SPCL_NA_CHAR_SET_CH_2_4))
+        && ((ccData2 & SPCL_NA_CHAR_SET_MASK) == ccData2)) {
+        LOG("Found CEA-608 Special Text at %d:%02d:%02d,%03d", captionTimePtr->hour,
+            captionTimePtr->minute, captionTimePtr->second, captionTimePtr->millisecond);
+        captionTextFound = CCX_TRUE;
+        return;
+    }
+
+    if( ((ccData1 == EXT_W_EURO_CHAR_SET_CH_1_3_SF) || (ccData1 == EXT_W_EURO_CHAR_SET_CH_1_3_FG) ||
+         (ccData1 == EXT_W_EURO_CHAR_SET_CH_2_4_SF) || (ccData1 == EXT_W_EURO_CHAR_SET_CH_2_4_FG))
+        && ((ccData2 & EXT_W_EURO_CHAR_SET_MASK) == ccData2)) {
+        LOG("Found CEA-608 Extended Text at %d:%02d:%02d,%03d", captionTimePtr->hour,
+            captionTimePtr->minute, captionTimePtr->second, captionTimePtr->millisecond);
+        captionTextFound = CCX_TRUE;
+        return;
+    }
+
+    if( (((ccData1 >= FIRST_BASIC_CHAR) && (ccData1 <= LAST_BASIC_CHAR)) || (ccData1 == NULL_BASIC_CHAR)) &&
+        (((ccData2 >= FIRST_BASIC_CHAR) && (ccData2 <= LAST_BASIC_CHAR)) || (ccData2 == NULL_BASIC_CHAR)) ) {
+        LOG("Found CEA-608 Basic Text at %d:%02d:%02d,%03d", captionTimePtr->hour,
+            captionTimePtr->minute, captionTimePtr->second, captionTimePtr->millisecond);
+        captionTextFound = CCX_TRUE;
+    }
+
+} // processLine21Packet()
+
+static void processDtvccPacket( ccx_mcc_caption_time* captionTimePtr ) {
+    uint8 packet_size_code = dtvccPacket[0] & PACKET_LENGTH_MASK;
+
+    if( dtvccPacketLength  == 0 ) {
+        return;
+    }
+
+    uint8 packet_size;
+    if( packet_size_code == 0 ) {
+        packet_size = 128;
+    } else {
+        packet_size = 2 * packet_size_code;
+    }
+
+    uint8* dataPtr = &dtvccPacket[1];
+    uint8 packetDataIndex = 0;
+
+    while( (packetDataIndex < packet_size) && ((packet_size - packetDataIndex) > 1) ) {               // packet_data_size = packet_size - 1
+        uint8 service_number = (dataPtr[packetDataIndex] & SERVICE_NUMBER_MASK) >> SERVICE_NUMBER_SHIFT;     // 3 more significant bits
+        uint8 block_size = (dataPtr[packetDataIndex] & SERVICE_BLOCK_SIZE_MASK);                             // 5 less significant bits
+
+        if( block_size == 0 ) break;
+
+        if( service_number == EXTENDED_SRV_NUM_PATTERN ) {
+            ASSERT(block_size);
+            packetDataIndex++;
+            service_number = (dataPtr[packetDataIndex] & EXTENDED_SRV_NUM_MASK);
+        }
+        packetDataIndex++; // Move to service data
+
+        if( service_number == 0 && block_size != 0 ) { // Illegal, but specs say what to do...
+            packetDataIndex = packet_size; // Move to end
+            break;
+        }
+
+        uint8 index = 0;
+
+        while( index < block_size ) {
+            char used = LENGTH_UNKNOWN;
+
+            if( dataPtr[index] != DTVCC_C0_EXT1 ) {
+                if( (dataPtr[index] >= DTVCC_MIN_C0_CODE) && (dataPtr[index] <= DTVCC_MAX_C0_CODE) ) {
+                    if( dataPtr[index] <= 0x0F ) used = 1;
+                    else if( dataPtr[index] <= 0x17 ) used = 2;
+                    else if( dataPtr[index] <= DTVCC_MAX_C0_CODE ) used = 3;
+                    else LOG( "Impossible Branch %02X", dataPtr[index] );
+                } else if( (dataPtr[index] >= DTVCC_MIN_G0_CODE) && (dataPtr[index] <= DTVCC_MAX_G0_CODE) ) {
+                    used = 1;
+                    LOG("Found CEA-708 G0 Text on Service %d at %d:%02d:%02d,%03d", service_number, captionTimePtr->hour,
+                            captionTimePtr->minute, captionTimePtr->second, captionTimePtr->millisecond);
+                    captionTextFound = CCX_TRUE;
+                } else if( (dataPtr[index] >= DTVCC_MIN_C1_CODE) && (dataPtr[index] <= DTVCC_MAX_C1_CODE) ) {
+                    if( (dataPtr[index] == DTVCC_C1_CW0) || (dataPtr[index] == DTVCC_C1_CW1) || (dataPtr[index] == DTVCC_C1_CW2) ||
+                        (dataPtr[index] == DTVCC_C1_CW3) || (dataPtr[index] == DTVCC_C1_CW4) || (dataPtr[index] == DTVCC_C1_CW5) ||
+                        (dataPtr[index] == DTVCC_C1_CW6) || (dataPtr[index] == DTVCC_C1_CW7) || (dataPtr[index] == DTVCC_C1_DLC) ||
+                        (dataPtr[index] == DTVCC_C1_RST) || (dataPtr[0] == DTVCC_C1_RSV93)   || (dataPtr[0] == DTVCC_C1_RSV94)   ||
+                        (dataPtr[0] == DTVCC_C1_RSV95)   || (dataPtr[0] == DTVCC_C1_RSV96)) {
+                        used = 1;
+                    } else if( (dataPtr[index] == DTVCC_C1_CLW) || (dataPtr[index] == DTVCC_C1_DSW) || (dataPtr[index] == DTVCC_C1_HDW) ||
+                               (dataPtr[index] == DTVCC_C1_TGW) || (dataPtr[index] == DTVCC_C1_DLW) || (dataPtr[index] == DTVCC_C1_DLW) ||
+                               (dataPtr[index] == DTVCC_C1_DLY) ) {
+                        used = 2;
+                    } else if( (dataPtr[index] == DTVCC_C1_SPA) || (dataPtr[index] == DTVCC_C1_SPL) ) {
+                        used = 3;
+                    } else if( dataPtr[index] == DTVCC_C1_SPC ) {
+                        used = 4;
+                    } else if( dataPtr[index] == DTVCC_C1_SWA ) {
+                        used = 5;
+                    } else if( (dataPtr[index] == DTVCC_C1_DF0) || (dataPtr[index] == DTVCC_C1_DF1) || (dataPtr[index] == DTVCC_C1_DF2) ||
+                               (dataPtr[index] == DTVCC_C1_DF3) || (dataPtr[index] == DTVCC_C1_DF4) || (dataPtr[index] == DTVCC_C1_DF5) ||
+                               (dataPtr[index] == DTVCC_C1_DF6) || (dataPtr[index] == DTVCC_C1_DF7) ) {
+                        used = 7;
+                    } else {
+                        LOG( "Impossible Branch: 0x%02X", dataPtr[0] );
+                        used = 1;
+                    }
+                } else {
+                    used = 1;
+                }
+                if( used == LENGTH_UNKNOWN ) {
+                    LOG( "There was a problem handling the data. Reseting service decoder" );
+                    return;
+                }
+            } else {  // Use Extended Set
+                if( (dataPtr[index+1] >= DTVCC_MIN_C0_CODE) && (dataPtr[index+1] <= DTVCC_MAX_C0_CODE) ) { // C2: Extended Misc. Control Codes
+                    if( dataPtr[index+2] < 0x07 ) { // 00-07 : Single-byte control bytes (0 additional bytes)
+                        used = 2;
+                    } else if( dataPtr[index+2] < 0x0F ) { // 08-0F : Two-byte control codes (1 additional byte)
+                        used = 3;
+                    } else if( dataPtr[index+2] < 0x0F ) { // 10-17 : Three-byte control codes (2 additional bytes)
+                        used = 4;
+                    } else {  // 18-1F : Four-byte control codes (3 additional bytes)
+                        used = 5;
+                    }
+                } else if( (dataPtr[index+1] >= DTVCC_MIN_G0_CODE) && (dataPtr[index+1] <= DTVCC_MAX_G0_CODE) ) {  // G2: Extended Misc. Characters
+                    used = 2;
+                    LOG("Found CEA-708 G2 Text on Service %d at %d:%02d:%02d,%03d", service_number, captionTimePtr->hour,
+                        captionTimePtr->minute, captionTimePtr->second, captionTimePtr->millisecond);
+                    captionTextFound = CCX_TRUE;
+                } else if( (dataPtr[index+1] >= DTVCC_MIN_C1_CODE) && (dataPtr[index+1] <= DTVCC_MAX_C1_CODE) ) {
+                    if( (dataPtr[index+2] < 0x80) || (dataPtr[index+2] > 0x9F) ) {
+                        LOG( "Skipping Invalid C3 Cmd: 0x%02X", dataPtr[index+2] );
+                    } else if( dataPtr[index+2] <= 0x87 ) { // 80-87 : Five-byte control bytes (4 additional bytes)
+                        used = 6;
+                    } else if( dataPtr[index+2] <= 0x8F ) { // 88-8F : Six-byte control codes (5 additional byte)
+                        used = 7;
+                    } else {
+                        // 90-9F : These are variable length commands, that can even span several segments.
+                        // They were envisioned for things like downloading fonts and graphics.
+                        // We are not supporting this set of data.
+                        LOG( "Likely Data Corruption. Unsupported C3 Data Range: 0x%02X", dataPtr[index+2] );
+                        return;
+                    }
+                } else {  // G3 Character Set (Basically just the [CC] Symbol).
+                    used = 2;
+                    LOG("Found CEA-708 G3 Text on Service %d at %d:%02d:%02d,%03d", service_number, captionTimePtr->hour,
+                        captionTimePtr->minute, captionTimePtr->second, captionTimePtr->millisecond);
+                    captionTextFound = CCX_TRUE;
+                }
+            }
+            index = index + used;
+        }
+        ASSERT(index == block_size);
+        packetDataIndex = packetDataIndex + block_size; // Skip data
+    }
+}  // processDtvccPacket()
--- src/lib_ccx/ccx_encoders_mcc.h
+++ src/lib_ccx/ccx_encoders_mcc.h
@@ -9,6 +9,8 @@
 /*--                               Constants                                --*/
 /*----------------------------------------------------------------------------*/
 
+// --- MCC Encode Related Constants
+
 #define ANC_DID_CLOSED_CAPTIONING        0x61
 #define ANC_SDID_CEA_708                 0x01
 #define CDP_IDENTIFIER_VALUE_HIGH        0x96
@@ -25,6 +27,107 @@
 #define CDP_FRAME_RATE_50                0x06
 #define CDP_FRAME_RATE_59_94             0x07
 #define CDP_FRAME_RATE_60                0x08
+
+// --- Caption Decode Related Constants
+
+#define CEA608E_LINE21_FIELD_1_CC        0x00
+#define CEA608E_LINE21_FIELD_2_CC        0x01
+#define DTVCCC_CHANNEL_PACKET_DATA       0x02
+#define DTVCCC_CHANNEL_PACKET_START      0x03
+
+#define CC_CONSTR_CC_VALID_FLAG_MASK     0x04
+#define CC_CONSTR_CC_VALID_FLAG_SET      0x04
+#define CC_CONSTR_CC_TYPE_MASK           0x03
+
+#define LINE_21_PARITY_MASK              0x7F
+
+#define GLOBAL_CTRL_CODE_CC1             0x14
+#define GLOBAL_CTRL_CODE_CC2             0x1C
+#define GLOBAL_CTRL_CODE_CC3             0x15
+#define GLOBAL_CTRL_CODE_CC4             0x1D
+#define GLOBAL_CTRL_CMD_MASK             0x2F
+
+#define CMD_MIDROW_BG_CHAN_1_3           0x10
+#define CMD_MIDROW_FG_CHAN_1_3           0x11
+#define CMD_MIDROW_BG_CHAN_2_4           0x18
+#define CMD_MIDROW_FG_CHAN_2_4           0x19
+#define MIDROW_CODE_MASK                 0x2F
+
+#define CMD_TAB_HI_CC_1_3                0x17
+#define CMD_TAB_HI_CC_2_4                0x1F
+#define TAB_CMD_MASK                     0x23
+
+#define PAC_CC1_MASK                     0x1F
+
+#define SPCL_NA_CHAR_SET_CH_1_3          0x11
+#define SPCL_NA_CHAR_SET_CH_2_4          0x19
+#define SPCL_NA_CHAR_SET_MASK            0x3F
+
+#define EXT_W_EURO_CHAR_SET_CH_1_3_SF    0x12
+#define EXT_W_EURO_CHAR_SET_CH_1_3_FG    0x13
+#define EXT_W_EURO_CHAR_SET_CH_2_4_SF    0x1A
+#define EXT_W_EURO_CHAR_SET_CH_2_4_FG    0x1B
+#define EXT_W_EURO_CHAR_SET_MASK         0x3F
+
+#define NULL_BASIC_CHAR                  0x00
+#define FIRST_BASIC_CHAR                 0x20
+#define LAST_BASIC_CHAR                  0x7F
+
+#define DTVCC_MAX_PACKET_LENGTH           128
+#define PACKET_LENGTH_MASK               0x3F
+
+#define SERVICE_NUMBER_MASK              0xE0
+#define SERVICE_NUMBER_SHIFT                5
+#define SERVICE_BLOCK_SIZE_MASK          0x1F
+
+#define EXTENDED_SRV_NUM_PATTERN         0x07
+#define EXTENDED_SRV_NUM_MASK            0x3F
+
+#define LENGTH_UNKNOWN                     -1
+
+#define DTVCC_C0_EXT1                    0x10
+
+#define DTVCC_MIN_G0_CODE                0x20
+#define DTVCC_MAX_G0_CODE                0x7F
+
+#define DTVCC_MIN_C0_CODE                0x00
+#define DTVCC_MAX_C0_CODE                0x1F
+
+#define DTVCC_C1_CW0                     0x80
+#define DTVCC_C1_CW1                     0x81
+#define DTVCC_C1_CW2                     0x82
+#define DTVCC_C1_CW3                     0x83
+#define DTVCC_C1_CW4                     0x84
+#define DTVCC_C1_CW5                     0x85
+#define DTVCC_C1_CW6                     0x86
+#define DTVCC_C1_CW7                     0x87
+#define DTVCC_C1_CLW                     0x88
+#define DTVCC_C1_DSW                     0x89
+#define DTVCC_C1_HDW                     0x8A
+#define DTVCC_C1_TGW                     0x8B
+#define DTVCC_C1_DLW                     0x8C
+#define DTVCC_C1_DLY                     0x8D
+#define DTVCC_C1_DLC                     0x8E
+#define DTVCC_C1_RST                     0x8F
+#define DTVCC_C1_SPA                     0x90
+#define DTVCC_C1_SPC                     0x91
+#define DTVCC_C1_SPL                     0x92
+#define DTVCC_C1_RSV93                   0x93
+#define DTVCC_C1_RSV94                   0x94
+#define DTVCC_C1_RSV95                   0x95
+#define DTVCC_C1_RSV96                   0x96
+#define DTVCC_C1_SWA                     0x97
+#define DTVCC_C1_DF0                     0x98
+#define DTVCC_C1_DF1                     0x99
+#define DTVCC_C1_DF2                     0x9A
+#define DTVCC_C1_DF3                     0x9B
+#define DTVCC_C1_DF4                     0x9C
+#define DTVCC_C1_DF5                     0x9D
+#define DTVCC_C1_DF6                     0x9E
+#define DTVCC_C1_DF7                     0x9F
+
+#define DTVCC_MIN_C1_CODE                0x80
+#define DTVCC_MAX_C1_CODE                0x9F
 
 /*----------------------------------------------------------------------------*/
 /*--                                Types                                   --*/
